6f5321bfa2ca8a6e20bb0e945507610ea3c95565
Muhammet <msolmaz83@gmail.com>
2024-04-03 02:21:24 +0200

    search

--- app/components/Navbar/Search.tsx
+++ app/components/Navbar/Search.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div className='flex-1 hidden md:flex'>Search</div>
+    <div className='flex-1 hidden md:flex'>
+      <input className='py-2 px-3 border-none outline-none flex flex-1 placeholder:"Search.....'/>
+      <button className='p-2 bg-orange-800 text-sm border border-transparent'>Search</button>
+      </div>
   )
 }
 
