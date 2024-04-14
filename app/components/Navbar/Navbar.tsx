894c292b489f54123920545a54ad195fbc0b4e1e
Muhammet <msolmaz83@gmail.com>
2024-04-15 01:12:31 +0200

    type_eslint

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Logo from './Logo'
 import Search from './Search'
 import CardCount from './CardCount'
@@ -6,37 +6,16 @@ import User from './User'
 import HamburgerMenu from './HamburgerMenu'
 import { getCurrentUser } from '@/app/actions/getCurrentUser'
 
-interface curProps {
-  createdAt: string;
-  updatedAt: string;
-  emailVerified: string | undefined;
-  id: string;
-  name: string | null;
-  email: string | null;
-  image: string | null;
-  hashedPassword: string | null;
-  updateAt: Date;
-  role?: $Enums.Role
-}
+// eslint-disable-next-line react/prop-types  
 
-const Navbar = () => {
-  const [currentUser, setCurrentUser] = useState<curProps | null>(null);
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      const user = await getCurrentUser();
-      setCurrentUser(user);
-    }
-
-    fetchUser();
-  }, []);
-
+const Navbar =  async () => {
+  const currentUser = await getCurrentUser()
   return (
     <div className='flex items-center justify-between gap-3 md:gap-10 px-3 md:px-10 h-16 bg-orange-400 '>
         <Logo/>
         <Search/>
-        {currentUser && <CardCount  currentUser = {currentUser}/>}
-        {currentUser && <User currentUser = {currentUser}/>}
+        <CardCount  currentUser = {currentUser}/>
+        <User currentUser = {currentUser}/>
         <HamburgerMenu/>
     </div>
   )
