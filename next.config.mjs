d31d3177aa02372796225bbb48da442394159f48
Muhammet <msolmaz83@gmail.com>
2024-04-17 00:18:38 +0200

    eslintignore

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images:{
         domains:["i.imgur.com","placeimg.com","via.placeholder.com","ejemplo.com","api.escuelajs.co","img.freepik.com","products.com","cdn.pixabay.com" ,
     "placeimg.com"]
+    },
+    eslint:{
+        ignoreDuringBuilds:true
     }
 };
 
